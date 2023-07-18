208311454d09b9c46749f4aa7dd58c7c43bc35e0
macie <macie@users.noreply.github.com>
2023-07-18 22:20:17 +0200

    feat: Handle missing git
    Git is the only one dependency which is not a basic unix command. It can be not
    available by default, so we need to be prepared.
    
    Fixes: #1

---
+++ tests/test_regression.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+beforeAll() {
+    TEST_ROOT_DIR=$(mktemp -d -t 'shinit_testXXXXXX')
+    MOCK_DIR=$(mktemp -d -t 'shinit_mockXXXXXX')
+
+    USED_CMDS='cat cd chmod curl date hexdump id ls mkdir printf sed tr'
+    for MOCKED_CMD in $USED_CMDS; do
+        echo '#!/bin/sh' >"${MOCK_DIR}/${MOCKED_CMD}"
+        if type "$MOCKED_CMD" | grep -q 'builtin'; then
+            echo 'builtin '"$MOCKED_CMD"' $@' >>"${MOCK_DIR}/${MOCKED_CMD}"
+        else
+            echo "$(command -v "$MOCKED_CMD")"' $@' >>"${MOCK_DIR}/${MOCKED_CMD}"
+        fi
+        chmod +x "${MOCK_DIR}/${MOCKED_CMD}"
+    done
+}
+
+afterAll() {
+    rm -r "${TEST_ROOT_DIR:-/tmp/shinit}" "${MOCK_DIR:-/tmp/shinit_mock}" 2>/dev/null
+}
+
+test_issue1() {
+    echo | PATH="$MOCK_DIR" ./shinit "$TEST_ROOT_DIR" 2>/dev/null >&2
+    test $? -eq 0
+
+    TEST_PROJ_DIR=$(find "$TEST_ROOT_DIR"/* -type d -prune -print | head -1)
+    TEST_PROJ_SCRIPT=$(basename "$TEST_PROJ_DIR")
+    # shellcheck disable=SC2012
+    test "$(ls -a "$TEST_PROJ_DIR" | tr '\n' ' ')" = ". .. LICENSE README.md $TEST_PROJ_SCRIPT " 
+}
